dfb8831f0e7679afc6df4a63a76d8335114290e2
dmitryame <dmitry@custommode.com>
2021-04-14 15:19:12 -0400

    sorting out the configs

--- lambda-fns/db.ts
+++ lambda-fns/db.ts
@@ -29,7 +29,7 @@ const db = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: process.env.DB_PORT,
 })
 
 export default db
