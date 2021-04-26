04557b7a2c34326b7391972957dca591124edb74
dmitryame <dmitry@custommode.com>
2021-04-25 21:57:51 -0400

    fixed eslint

--- tests/abuserReport.js
+++ tests/abuserReport.js
@@ -3,10 +3,10 @@ import uuid from 'uuid'
 import supertest from 'supertest'
 import chai from 'chai'
 
-import { config } from '../../../.env.test'
+import {config,} from '../.env.test'
 
 const request = supertest(config().HOST)
-const { expect } = chai // BDD/TDD assertion library
+const {expect,} = chai // BDD/TDD assertion library
 
 describe('abusereport', () => {
   it('should not be able to post an abuseReport with no parameters', async () => {
@@ -24,9 +24,9 @@ describe('abusereport', () => {
 
     const response =
     await request
-      .post('/abusereport')
-      .set('Content-Type', 'application/json')
-      .send({ uuid: guid })
+    	.post('/abusereport')
+    	.set('Content-Type', 'application/json')
+    	.send({uuid: guid,})
 
     expect(response.status).to.equal(201)
     expect(response.body.status).to.equal('success')
@@ -36,32 +36,32 @@ describe('abusereport', () => {
   it('should not be able to post a photo wben too many abuses reported', async () => {
     const guid = uuid()
 
-    const point = { type: 'Point', coordinates: [-29.396377, -137.585190] }
+    const point = {type: 'Point', coordinates: [-29.396377, -137.585190,],}
     // console.log("contents.size: ", contents.length)
     // post 4 abuse reports for a particular UUID
     await request
       .post('/abusereport')
       .set('Content-Type', 'application/json')
-      .send({ uuid: guid })
+      .send({uuid: guid,})
     await request
       .post('/abusereport')
       .set('Content-Type', 'application/json')
-      .send({ uuid: guid })
+      .send({uuid: guid,})
     await request
       .post('/abusereport')
       .set('Content-Type', 'application/json')
-      .send({ uuid: guid })
+      .send({uuid: guid,})
     await request
       .post('/abusereport')
       .set('Content-Type', 'application/json')
-      .send({ uuid: guid })
+      .send({uuid: guid,})
 
     const response =
     await request
-      .post('/photos')
-      .set('Content-Type', 'application/json')
-      .send({ uuid: guid })
-      .send({ location: point })
+    	.post('/photos')
+    	.set('Content-Type', 'application/json')
+    	.send({uuid: guid,})
+    	.send({location: point,})
 
     expect(response.status).to.equal(401)
     expect(response.body.error).to.equal('Anauthorized.')
