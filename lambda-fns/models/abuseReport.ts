9f67a877b48fe066e67444c7c019ee086f93d294
dmitryame <dmitry@custommode.com>
2021-05-08 14:32:17 -0400

    wiring up abuseReport

--- lambda-fns/models/abuseReport.ts
+++ lambda-fns/models/abuseReport.ts
@@ -1,33 +1,9 @@
-import Sequelize from 'sequelize'
-import { sequelize } from '../../../config/consts'
-
-const AbuseReport = sequelize.define('AbuseReport', {
-  id: {
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-    type: Sequelize.INTEGER,
-  },
-  photoId: {
-    allowNull: false,
-    type: Sequelize.INTEGER,
-  },
-  uuid: {
-    type: Sequelize.UUID,
-    allowNull: false,
-  },
-  createdAt: {
-    allowNull: false,
-    type: Sequelize.DATE,
-  },
-  updatedAt: {
-    allowNull: false,
-    type: Sequelize.DATE,
-  },
-})
-
-// Adding a class level method
-
-// Adding an instance level method
+type AbuseReport = {
+  id: bigint
+  photoId: bigint
+  uuid: string
+  createdAt: string
+  updatedAt: string
+}
 
 export default AbuseReport
