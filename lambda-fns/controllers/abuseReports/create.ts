c93a6538df74838a901066d8c03179ec65aa5b3b
dmitryame <dmitry@custommode.com>
2021-05-20 18:37:38 -0400

    abuse report graphql works

--- lambda-fns/controllers/abuseReports/create.ts
+++ lambda-fns/controllers/abuseReports/create.ts
@@ -10,23 +10,22 @@ import AbuseReport from '../../models/abuseReport'
 export default async function main(abuseReport: AbuseReport) {
   abuseReport.createdAt = moment().format("YYYY-MM-DD HH:mm:ss.SSS")
   abuseReport.updatedAt = abuseReport.createdAt
-  console.log({abuseReport})
-    try {
-        return (await sql`
-                          insert into "AbuseReports"
-                          (
-                              "photoId",
-                              "uuid",
-                              "createdAt",
-                              "updatedAt"
-                          ) values (
-                            ${abuseReport.photoId},
-                            ${abuseReport.uuid},
-                            ${abuseReport.createdAt},
-                            ${abuseReport.updatedAt}
-                          )
-                          returning *
-                          `)[0]
+  try {
+    return (await sql`
+                      insert into "AbuseReports"
+                      (
+                          "photoId",
+                          "uuid",
+                          "createdAt",
+                          "updatedAt"
+                      ) values (
+                        ${abuseReport.photoId},
+                        ${abuseReport.uuid},
+                        ${abuseReport.createdAt},
+                        ${abuseReport.updatedAt}
+                      )
+                      returning *
+                      `)[0]
     } catch (error) {
         console.log({error})
         return null
