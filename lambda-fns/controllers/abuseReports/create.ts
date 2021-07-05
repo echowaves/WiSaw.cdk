714e7473c2acc4c5d4710ce24c848c607d09f107
dmitryame <dmitry@custommode.com>
2021-07-04 20:21:10 -0400

    abuse report finalized

--- lambda-fns/controllers/abuseReports/create.ts
+++ lambda-fns/controllers/abuseReports/create.ts
@@ -6,26 +6,21 @@ import sql from '../../sql'
 
 export default async function main(photoId: bigint, uuid: string) {
   const createdAt = moment().format("YYYY-MM-DD HH:mm:ss.SSS")
-  try {
-    const result = (await sql`
-                      insert into "AbuseReports"
-                      (
-                          "photoId",
-                          "uuid",
-                          "createdAt",
-                          "updatedAt"
-                      ) values (
-                        ${photoId},
-                        ${uuid},
-                        ${createdAt},
-                        ${createdAt}
-                      )
-                      returning *
-                      `)
-                      // console.log({result})
-                      return result[0]
-    } catch (error) {
-        console.log({error})
-        return null
-    }
+  const result = (await sql`
+                    insert into "AbuseReports"
+                    (
+                        "photoId",
+                        "uuid",
+                        "createdAt",
+                        "updatedAt"
+                    ) values (
+                      ${photoId},
+                      ${uuid},
+                      ${createdAt},
+                      ${createdAt}
+                    )
+                    returning *
+                    `
+                  )
+  return result[0]
 }
--- lambda-fns/controllers/abuseReports/list.ts
+++ lambda-fns/controllers/abuseReports/list.ts
@@ -3,11 +3,5 @@ import sql from '../../sql'
 import AbuseReport from '../../models/abuseReport'
 
 export default async function main() {
-
-    try {
-        return await sql`select * from "AbuseReports"`
-    } catch (error) {
-        console.log({error})
-        return null
-    }
+    return await sql`select * from "AbuseReports"`
 }
