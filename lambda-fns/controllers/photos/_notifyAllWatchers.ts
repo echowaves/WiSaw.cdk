36bda43e1fc8608bc767847e281aafc8394c97ba
dmitryame <dmitry@custommode.com>
2021-07-31 21:53:56 -0400

    done: watchersCount

--- lambda-fns/controllers/photos/_notifyAllWatchers.ts
+++ lambda-fns/controllers/photos/_notifyAllWatchers.ts
@@ -5,14 +5,14 @@ import sql from '../../sql'
 // update "Photos" set "watchersCount" = (select count(*) from "Watchers" where "photoId" = "Photos".id and uuid != "Photos"."uuid");
 // select distinct "watchersCount" from "Photos" order by "watchersCount" DESC;
 
-export const _notifyAllWatchers = async( photoId: bigint) => {  
+export const _notifyAllWatchers = async( photoId: bigint) => {
   // this is when it happens
   const watchedAt = moment().format("YYYY-MM-DD HH:mm:ss.SSS");
 
   (await sql`
     UPDATE "Watchers"
-      SET "watchedAt" = ${watchedAt}
-    WHERE id = ${photoId}
+    SET "watchedAt" = ${watchedAt}
+    WHERE "photoId" = ${photoId}
     `)
   return
 }
