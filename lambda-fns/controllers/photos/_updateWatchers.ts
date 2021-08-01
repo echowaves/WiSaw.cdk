598925daa837a4daf5305885389edae6eea1c0d7
dmitryame <dmitry@custommode.com>
2021-07-31 21:17:09 -0400

    done: watchersCount

--- lambda-fns/controllers/photos/_updateWatchers.ts
+++ lambda-fns/controllers/photos/_updateWatchers.ts
@@ -2,6 +2,9 @@ import * as moment from 'moment'
 
 import sql from '../../sql'
 
+// update "Photos" set "watchersCount" = (select count(*) from "Watchers" where "photoId" = "Photos".id and uuid != "Photos"."uuid");
+// select distinct "watchersCount" from "Photos" order by "watchersCount" DESC;
+
 export const _updateWatchers = async( photoId: bigint, uuid: string) => {
 
   const count = (await sql`
--- lambda-fns/controllers/photos/watch.ts
+++ lambda-fns/controllers/photos/watch.ts
@@ -4,8 +4,6 @@ import sql from '../../sql'
 
 import {_updateWatchers} from './_updateWatchers'
 
-// update "Photos" set "watchersCount" = (select count(*) from "Watchers" where "photoId" = "Photos".id and uuid != "Photos"."uuid");
-
 export default async function main(photoId: bigint, uuid: string) {
 
   const createdAt = moment().format("YYYY-MM-DD HH:mm:ss.SSS")
