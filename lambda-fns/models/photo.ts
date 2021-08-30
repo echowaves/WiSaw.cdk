65fda6ea635a21c274310c918d4e3058c29ffe03
dmitryame <dmitry@custommode.com>
2021-08-30 17:28:18 -0400

    changing file extention to mov

--- lambda-fns/models/photo.ts
+++ lambda-fns/models/photo.ts
@@ -18,7 +18,7 @@ class Photo {
        // thumbUrl: `https://s3.amazonaws.com/${process.env.S3_BUCKET}/${this.id}-thumb`,
        imgUrl: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${this.id}`,
        thumbUrl: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${this.id}-thumb`,
-       videoUrl: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${this.id}.vid`,
+       videoUrl: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${this.id}.mov`,
 		};
 	}
 }
