085b43f87fb1d6ea15d18a18790811f63c7d0378
dmitryame <dmitry@custommode.com>
2021-09-09 12:04:23 -0400

    increasing expiration time for the upload link to 60 minutes

--- lambda-fns/controllers/photos/generateUploadUrl.ts
+++ lambda-fns/controllers/photos/generateUploadUrl.ts
@@ -12,7 +12,7 @@ export default async function main(assetKey: string, contentType: string) {
     Bucket: process.env.S3_BUCKET,
     Key: `${assetKey}`,
     ContentType: contentType,
-    Expires: 60, // expires in 1 minute, after that request a new URL
+    Expires: 60 * 60, // expires in 1 minute * 60 minutes, after that request a new URL
     ACL: 'public-read',
   }
 
