9813eb5990523535f90fced3a33b241dc9daf8fc
dmitryame <dmitry@custommode.com>
2021-11-07 17:05:43 -0500

    added migration

---
+++ migrations/20211107163000-create-secret.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Secrets', {
+      uuid: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+      },
+      nickName: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      secret: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+      .then(() => queryInterface.addIndex('Secrets', ['nickName',]))
+      // .then(() => queryInterface.addIndex('Secrets', ['secret',])) // will not search by password
+      // .then(() => queryInterface.addIndex('Secrets', ['createdAt',]))
+  ,
+  down: (queryInterface, Sequelize) => // eslint-disable-line no-unused-vars
+    queryInterface.dropTable('Secrets'),
+}
