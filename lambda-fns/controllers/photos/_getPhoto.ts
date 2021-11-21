a3c96ec62dd041e511fddf8332a270d4f21a58e1
dmitryame <dmitry@custommode.com>
2021-11-21 11:28:09 -0500

    wip: wiring up friends and chats methods

--- lambda-fns/controllers/photos/_getPhoto.ts
+++ lambda-fns/controllers/photos/_getPhoto.ts
@@ -1,15 +1,17 @@
-import { plainToClass } from 'class-transformer'
+/* eslint-disable import/named */
+import {plainToClass,} from 'class-transformer'
+
 import sql from '../../sql'
 import Photo from '../../models/photo'
 
 export const _getPhoto = async( photoId: bigint) => {
-  const result =  (await sql`
+  const result = (await sql`
                     SELECT * FROM "Photos"
                     WHERE
                       "id" = ${photoId}
                     LIMIT 1
                     `
-                  )[0]
-    const photo = plainToClass(Photo, result)
-    return photo
+  )[0]
+  const photo = plainToClass(Photo, result)
+  return photo
 }
