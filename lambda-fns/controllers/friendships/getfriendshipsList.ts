9d4c9a1ec24c15df88b52d9edb82d41ec75ba824
dmitryame <dmitry@custommode.com>
2021-11-23 21:40:50 -0500

    friendship simplified a bit

--- lambda-fns/controllers/friendships/getfriendshipsList.ts
+++ lambda-fns/controllers/friendships/getfriendshipsList.ts
@@ -13,19 +13,9 @@ export default async function main(
   }
 
   const friendships = (await sql`SELECT *
-      FROM "Friends"
-      WHERE "friendshipUuid" = ${uuid}
+      FROM "Friendships"
+      WHERE "uuid1" = ${uuid}
+      OR "uuid2" = ${uuid}
     `)
-
-  // if(friends.length !== 2) {
-  //   throw new Error(`Friendship is not established`)
-  // }
-
-  // const filteredFriends = friends.filter((friend: any) => friend.uuid !== uuid)
-  // if(filteredFriends.length >= 2) {
-  //   throw new Error(`Too many friends in this Friendship`)
-  // }
-
   return friendships.map((friendship: Friendship) => plainToClass(Friendship, friendship))
-
 }
