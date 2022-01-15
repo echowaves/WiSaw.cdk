1ab3ff770c5122bada0a5100fc50a07db3cc3366
dmitryame <dmitry@custommode.com>
2022-01-15 10:45:49 -0500

    ChatPhotos table added

--- migrations/20220109115200-messages-image.js
+++ migrations/20220109115200-messages-image.js
@@ -2,29 +2,55 @@ module.exports = {
   up: (queryInterface, Sequelize) =>
     queryInterface.addColumn(
       'Messages',
-      'image',
+      'pending',
       {
-        type: Sequelize.STRING,
-        allowNull: true,
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
       },
     )
       .then(() =>
         queryInterface.addColumn(
           'Messages',
-          'pending',
+          'chatPhotoUuid',
           {
-            type: Sequelize.BOOLEAN,
-            allowNull: false,
-            defaultValue: false,
+            type: Sequelize.UUID,
+            allowNull: true,
           },
-        )
-      )
-      .then(() => queryInterface.addIndex('Messages', ['image',]))
+        ))
+
+      .then(() => queryInterface.createTable('ChatPhotos', {
+        chatPhotoUuid: {
+          type: Sequelize.UUID,
+          primaryKey: true,
+          allowNull: false,
+        },
+        uuid: {
+          type: Sequelize.UUID,
+          allowNull: false,
+        },
+        contentHash: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('now'),
+        },
+      }))
+      .then(() => queryInterface.addIndex('ChatPhotos', ['uuid',]))
+      .then(() => queryInterface.addIndex('ChatPhotos', ['contentHash',]))
   ,
 
-  down: (queryInterface, Sequelize) => // eslint-disable-line no-unused-vars
-    queryInterface.removeColumn('Messages', 'image')
+  down:(queryInterface, Sequelize) =>
+    queryInterface.dropTable('ChatPhotos')
       .then(() =>
         queryInterface.removeColumn('Messages', 'pending')
-      ),
+      )
+      .then(() =>
+        queryInterface.removeColumn('Messages', 'chatPhotoUuid')
+      )
+  ,
 }
