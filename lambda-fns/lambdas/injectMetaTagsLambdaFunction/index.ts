e5c6252291e39d1b3f8191129b20e0ee69185c95
dmitryame <dmitry@custommode.com>
2022-06-18 08:59:28 -0400

    making progress on og tags

--- lambda-fns/lambdas/injectMetaTagsLambdaFunction/index.ts
+++ lambda-fns/lambdas/injectMetaTagsLambdaFunction/index.ts
@@ -29,36 +29,8 @@ export async function main(event: any = {}, context: any, callback: any) {
   
 
   const index = 
-`<\!DOCTYPE html>
-<html lang="en" prefix="og: http://ogp.me/ns#" xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#">
-  <head>    
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta name="google-site-verification" content="RQGZzEN0xtT0w38pKeQ1L8u8P6dn7zxfu03jt0LGgF4" />
-    <link rel="preconnect" href="https://www.wisaw.com" />
-    <link rel="preconnect" href="https://s3.amazonaws.com" />
-
-    <link rel="manifest" href="https://www.wisaw.com/manifest.json" />
-    <link rel="apple-touch-icon" sizes="180x180" href="https://www.wisaw.com/apple-touch-icon.webp" />
-    <link rel="icon" type="image/webp" href="https://www.wisaw.com/favicon-32x32.webp" sizes="32x32" />
-    <link rel="icon" type="image/webp" href="https://www.wisaw.com/favicon-16x16.webp" sizes="16x16" />
-    <link rel="mask-icon" href="https://www.wisaw.com/safari-pinned-tab.svg" color="#5bbad5" />
-
-    <meta name="theme-color" content="#ffffff" />
-    <link
-    rel="preload"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
-    as="style"
-    integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"
-    onload="this.onload=null;this.rel='stylesheet'" />
-  </head>
-  <body>
-    <noscript>
-      You need to enable JavaScript to run this app.
-    </noscript>
-    <div id="root"></div>
-  </body>
-</html>
+`
+<\!doctype html><html lang="en" prefix="og: http://ogp.me/ns#" xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#"><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="google-site-verification" content="RQGZzEN0xtT0w38pKeQ1L8u8P6dn7zxfu03jt0LGgF4"/><link rel="preconnect" href="https://www.wisaw.com"/><link rel="preconnect" href="https://s3.amazonaws.com"/><link rel="manifest" href="/manifest.json"/><link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.webp"/><link rel="icon" type="image/webp" href="/favicon-32x32.webp" sizes="32x32"/><link rel="icon" type="image/webp" href="/favicon-16x16.webp" sizes="16x16"/><link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/><meta name="theme-color" content="#ffffff"/><link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" as="style" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" onload='this.onload=null,this.rel="stylesheet"'/><script defer="defer" src="/static/js/main.8ee2345d.js"></script><link href="/static/css/main.e548762f.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html>
 `
 
   const body =  index.replace('<head>', 
