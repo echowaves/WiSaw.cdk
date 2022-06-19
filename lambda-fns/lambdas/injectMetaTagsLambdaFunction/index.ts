1506e92bc17d7f9bcd4af0237aae0c69bf285ac2
dmitryame <dmitry@custommode.com>
2022-06-19 17:38:45 -0400

    small og tag adjustment

--- lambda-fns/lambdas/injectMetaTagsLambdaFunction/index.ts
+++ lambda-fns/lambdas/injectMetaTagsLambdaFunction/index.ts
@@ -12,14 +12,14 @@ export async function main(event: any = {}, context: any, callback: any) {
 
   const body =  index.replace('<head>', 
   `<head>
-    <meta name='description' property='og:description' content='Check out What I saw Today' />
-    <meta property='og:title' content='wisaw photo ${imageId}' />
-    <meta property='og:url' content='https://www.wisaw.com/photos/${imageId}' />
-    <meta name='image' property='og:image' content='https://s3.amazonaws.com/wisaw-img-prod/${imageId}' />
-    <link rel='canonical' href='https://www.wisaw.com/photos/${imageId}' />
-    <meta name='twitter:title' content='wisaw (What I Saw) photo ${imageId}' />
-    <meta name='twitter:card' content='summary_large_image' />
-    <meta name='twitter:image' content='https://s3.amazonaws.com/wisaw-img-prod/${imageId}' />
+    <meta name="description" property="og:description" content="Check out What I saw Today">
+    <meta property="og:title" content="wisaw photo ${imageId}">
+    <meta property="og:url" content="https://www.wisaw.com/photos/${imageId}">
+    <meta name="image" property="og:image" content="https://s3.amazonaws.com/wisaw-img-prod/${imageId}">
+    <link rel="canonical" href="https://www.wisaw.com/photos/${imageId}">
+    <meta name="twitter:title" content="wisaw (What I Saw) photo ${imageId}">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:image" content="https://s3.amazonaws.com/wisaw-img-prod/${imageId}">
 `
   )
 
