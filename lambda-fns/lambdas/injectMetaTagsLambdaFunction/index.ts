2b28f237b5b4b4ef52db5c8209b6395e3ad5b5ab
dmitryame <dmitry@custommode.com>
2022-06-19 18:37:18 -0400

    small og tag adjustment

--- lambda-fns/lambdas/injectMetaTagsLambdaFunction/index.ts
+++ lambda-fns/lambdas/injectMetaTagsLambdaFunction/index.ts
@@ -12,7 +12,7 @@ export async function main(event: any = {}, context: any, callback: any) {
 
   const body =  index.replace('<head>', 
   `<head>
-    <meta name="image" property="og:image" content="https://s3.amazonaws.com/wisaw-img-prod/${imageId}" />
+    <meta name="image" property="og:image" content="https://wisaw-img-prod.s3.amazonaws.com/${imageId}" />
     <meta name="description" property="og:description" content="Check out What I saw Today" />
     <meta property="og:title" content="wisaw photo ${imageId}" />
     <meta property="og:url" content="https://www.wisaw.com/photos/${imageId}" />
@@ -20,7 +20,7 @@ export async function main(event: any = {}, context: any, callback: any) {
     <link rel="canonical" href="https://www.wisaw.com/photos/${imageId}" />
     <meta name="twitter:title" content="wisaw (What I Saw) photo ${imageId}" />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content="https://s3.amazonaws.com/wisaw-img-prod/${imageId}" />
+    <meta name="twitter:image" content="https://wisaw-img-prod.s3.amazonaws.com/${imageId}" />
 `
   )
 
