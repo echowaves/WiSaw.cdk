255c0f46bc2c03903b63e05c24cab2755be7b0b8
dmitryame <dmitry@custommode.com>
2025-06-26 14:02:04 -0400

    fix: update image URLs to use .webp format for improved performance

--- lambda-fns/models/photo.ts
+++ lambda-fns/models/photo.ts
@@ -15,8 +15,8 @@ class Photo {
       ...this,
       // imgUrl: `https://s3.amazonaws.com/${process.env.S3_BUCKET}/${this.id}`,
       // thumbUrl: `https://s3.amazonaws.com/${process.env.S3_BUCKET}/${this.id}-thumb`,
-      imgUrl: `https://${process.env.S3_IMAGES}/${this.id}`,
-      thumbUrl: `https://${process.env.S3_IMAGES}/${this.id}-thumb`,
+      imgUrl: `https://${process.env.S3_IMAGES}/${this.id}.webp`,
+      thumbUrl: `https://${process.env.S3_IMAGES}/${this.id}-thumb.webp`,
       videoUrl: `https://${process.env.S3_IMAGES}/${this.id}.mov`,
     }
   }
