0111f10cdd528d4f8102c447fd7c102f5c860ff6
dmitryame <dmitry@custommode.com>
2025-07-28 19:42:28 -0400

    feat: add width and height columns to Photos table for image dimensions

---
+++ migrations/20250728194101-add-photo-dimensions.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn(
+        'Photos',
+        'width',
+        {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+          comment: 'Image width in pixels',
+        },
+      ),
+      queryInterface.addColumn(
+        'Photos',
+        'height',
+        {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+          comment: 'Image height in pixels',
+        },
+      ),
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => { // eslint-disable-line no-unused-vars
+    return Promise.all([
+      queryInterface.removeColumn('Photos', 'width'),
+      queryInterface.removeColumn('Photos', 'height'),
+    ]);
+  },
+};
