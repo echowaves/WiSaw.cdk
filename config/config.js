5d05192655d74cf42149c63046caf2d91f2a5651
dmitryame <dmitry@custommode.com>
2025-11-20 20:20:22 -0500

    refactor: migrate config.js to ES module syntax; update SSL options for improved security

--- config/config.js
+++ config/config.js
@@ -1,45 +1,46 @@
-const devConfig = require("../.env.sample").config()
-const testConfig = require("../.env.test").config()
-const prodConfig = require("../.env.prod").config()
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
 
-module.exports = {
-  dev: {
-    username: devConfig.username,
-    password: devConfig.password,
-    database: devConfig.database,
-    host: devConfig.host,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        sslmode: "require",
-        rejectUnauthorized: false,
-      },
-    },
-  },
-  test: {
-    username: testConfig.username,
-    password: testConfig.password,
-    database: testConfig.database,
-    host: testConfig.host,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        sslmode: "require",
-        rejectUnauthorized: false,
-      },
-    },
-  },
-  prod: {
-    username: prodConfig.username,
-    password: prodConfig.password,
-    database: prodConfig.database,
-    host: prodConfig.host,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        sslmode: "require",
-        rejectUnauthorized: false,
-      },
-    },
-  },
+const devConfig = require('../.env.sample').config()
+const testConfig = require('../.env.test').config()
+const prodConfig = require('../.env.prod').config()
+
+export const dev = {
+  username: devConfig.username,
+  password: devConfig.password,
+  database: devConfig.database,
+  host: devConfig.host,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true
+    }
+  }
+}
+export const test = {
+  username: testConfig.username,
+  password: testConfig.password,
+  database: testConfig.database,
+  host: testConfig.host,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true
+    }
+  }
+}
+export const prod = {
+  username: prodConfig.username,
+  password: prodConfig.password,
+  database: prodConfig.database,
+  host: prodConfig.host,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true
+    }
+  }
 }
