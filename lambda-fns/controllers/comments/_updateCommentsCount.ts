ed342ac9be6aeae3177259ee8688a7abd49bb5f6
dmitryame <dmitry@custommode.com>
2025-12-29 14:10:02 -0500

    feat: updateCommentsCount function to include updatedAt timestamp in photo update query

--- lambda-fns/controllers/comments/_updateCommentsCount.ts
+++ lambda-fns/controllers/comments/_updateCommentsCount.ts
@@ -1,13 +1,16 @@
 import psql from '../../psql'
+import moment from 'moment'
 
 export const _updateCommentsCount = async( photoId: string) => {
+  const updatedAt = moment().format('YYYY-MM-DD HH:mm:ss.SSS')
   await psql.connect()
   const photo =
   (await psql.query(
     `UPDATE "Photos" SET "commentsCount" =
-      (SELECT COUNT(*) from "Comments" where "Comments"."photoId" = $1 and active = true)
+      (SELECT COUNT(*) from "Comments" where "Comments"."photoId" = $1 and active = true),
+      "updatedAt" = $3
       where id = $2
-      returning *`, [photoId, photoId])
+      returning *`, [photoId, photoId, updatedAt])
   ).rows[0]
   await psql.clean()
   return photo.commentsCount
